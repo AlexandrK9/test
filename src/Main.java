1725a0aa2ce6c629b32ee9a541e2afaed0d47880
AlexanderK <a_kirichuk@mail.ru>
2025-12-09 18:27:47 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        String stringTest = "\\bЛ[а-яА-Я]+\\b";
+        String text = "Рекс Лорд Лом Шарик Бобик Шалун";
+        Pattern pattern = Pattern.compile(stringTest);
+        Matcher matcher = pattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println("Нашли слово" + " " + matcher.group());
+        }
+    }
+}
